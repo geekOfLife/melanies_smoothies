57c25b7a85a2888a28983c8f504a5a5813aae022
geekOfLife <danny.betchetti@gmail.com>
2026-09-10 09:04:26 +0100

    Update streamlit_app.py
    Added dataframe to display when fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
@@ -31,6 +32,9 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
@@ -42,8 +46,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your smoothie has been ordered', icon="✅")
-
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
